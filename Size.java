4fa884003aa46fa79f87f90031ddd14c3ddd884f
jeffreylaichunghang <laichunghangjeffrey@gmail.com>
2024-02-25 23:08:02 +0800

    enum string

--- Size.java
+++ Size.java
@@ -1,5 +1,11 @@
-public enum Size {
-    SMALL("This size is small"), MEDIUM("This size is medium"), LARGE("This size is large"),
+enum Size {
+    SMALL("This size is small"),
+    MEDIUM("This size is medium"),
+    LARGE("This size is large") {
+        public String toString() {
+            return "This size is large";
+        };
+    },
     EXTRALARGE("This size is extra large");
 
     private final String size;
@@ -27,11 +33,12 @@ public enum Size {
     }
 
     public static void main(String[] args) {
-        System.out.println("The size is " + Size.SMALL.getSize());
-        System.out.println(Size.SMALL.ordinal());
-        System.out.println(Size.SMALL.compareTo(MEDIUM));
+        // System.out.println("The size is " + Size.SMALL.getSize());
+        // System.out.println(Size.SMALL.ordinal());
+        // System.out.println(Size.SMALL.compareTo(MEDIUM));
         System.out.println(Size.SMALL.toString());
-        System.out.println(Size.SMALL.name());
-        System.out.println(Size.values());
+        System.out.println(Size.LARGE.toString());
+        // System.out.println(Size.SMALL.name());
+        // System.out.println(Size.values());
     }
 }
