ef9b31537348bf9d531b89b45d6d671e9d30e75f
jeffreylaichunghang <laichunghangjeffrey@gmail.com>
2024-03-12 18:03:44 +0800

    blocking queue

---
+++ collection_interface/Queue/BlockingQueueEx.java
@@ -0,0 +1,28 @@
+package collection_interface.Queue;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ArrayBlockingQueue;
+/*
+ * Interface BlockingQueue
+ *  - ArrayBlockingQueue
+ *  - LinkedBlockingQueue
+ */
+
+public class BlockingQueueEx {
+    public static void main(String[] args) {
+        BlockingQueue<Integer> numbers = new ArrayBlockingQueue<>(5);
+
+        try {
+            numbers.put(2);
+            numbers.put(1);
+            numbers.put(3);
+            System.out.println("BlockingQueue: " + numbers);
+
+            int removedNumber = numbers.take();
+            System.out.println("remove number: " + removedNumber);
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.getStackTrace();
+        }
+    }
+}
